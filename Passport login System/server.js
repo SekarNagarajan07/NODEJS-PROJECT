18c58ae757865414eb15903173fe07e751a2e9d2
sekar N <sekarnagarajan1234@gmail.com>
2023-06-19 15:38:27 +0530

    node js

--- Passport login System/server.js
+++ Passport login System/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const req = require("express/lib/request");
 const app = express();
+const bcrypt = require("bcrypt");
+
+const users = [];
 
 app.set("view-engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.render("index.ejs", { name: "kanna" });
@@ -12,10 +16,22 @@ app.get("/login", (req, res) => {
   res.render("login.ejs");
 });
 
+app.post("/login", (req, res) => {});
+
 app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
 
-app.post("/register", (req, res) => {});
+app.post("/register", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+  } catch {}
+});
 
 app.listen(3000);
