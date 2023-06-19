5682c180ace579956847ebd64f9006ddbc8dd18e
sekar N <sekarnagarajan1234@gmail.com>
2023-06-19 17:35:14 +0530

    Node js

--- Passport login System/passport-config.js
+++ Passport login System/passport-config.js
@@ -1,7 +1,7 @@
 const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcrypt");
 
-function initialize(passport, getUserByEmail) {
+function initialize(passport, getUserByEmail, getUserById) {
   const authenticateUser = async (email, password, done) => {
     const user = getUserByEmail(email);
 
@@ -21,8 +21,10 @@ function initialize(passport, getUserByEmail) {
   };
 
   passport.use(new localStorage({ usernameField: "email" }), authenticateUser);
-  passport.serialUser((user, done) => {});
-  passport.deserializerUser((id, done) => {});
+  passport.serialUser((user, done) => done(null, user.id));
+  passport.deserializerUser((id, done) => {
+    return done(null, getUserById(id));
+  });
 }
 
 module.exports = initialize;
--- Passport login System/server.js
+++ Passport login System/server.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const req = require("express/lib/request");
 const app = express();
@@ -8,24 +12,44 @@ const session = require("express-session");
 
 const initializePassport = require("./passport-config");
 const passport = require("passport");
-initializePassport(passport, (email) =>
-  users.find((user) => user.email === email)
+initializePassport(
+  passport,
+  (email) => users.find((user) => user.email === email),
+  (id) => users.find((user) => user.id === id)
 );
 
 const users = [];
 
 app.set("view-engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/", (req, res) => {
-  res.render("index.ejs", { name: "kanna" });
+  res.render("index.ejs", { name: req.user.name });
 });
 
 app.get("/login", (req, res) => {
   res.render("login.ejs");
 });
 
-app.post("/login", (req, res) => {});
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
 
 app.get("/register", (req, res) => {
   res.render("register.ejs");
