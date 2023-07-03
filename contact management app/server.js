20f12d0d1f2440d5364a04efb37a5471737d4b48
sekar N <sekarnagarajan1234@gmail.com>
2023-07-03 21:50:51 +0530

    Node js

--- contact management app/server.js
+++ contact management app/server.js
@@ -1,1 +1,12 @@
-console.log("hello");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get("/api/contacts", (req, res) => {
+  res.send("Get all contacts");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
